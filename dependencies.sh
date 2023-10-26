fbd6e3b1d6ca9fb4d6d3f16d81130deb9822879a
AntoniaW <44059297+AntoniaSch@users.noreply.github.com>
2023-10-26 16:46:04 +0200

    Updating langchain version

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pip install --no-build-isolation --force-reinstall "boto3>=1.28.57" "awscli>=1.29.57" "botocore>=1.31.57"
-pip install --no-build-isolation --force-reinstall "langchain==0.0.263" "streamlit==1.26.0"
+pip install --no-build-isolation --force-reinstall "langchain>=0.0.318" "streamlit==1.26.0"
